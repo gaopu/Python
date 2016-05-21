a728771f067c21393ad05f09536154274186b296
geekgao <936302529@qq.com>
2016-05-21 08:48:00 +0800

    bing主页图片调整，代码也作了改变

--- SetBGPhoto.py
+++ SetBGPhoto.py
@@ -7,13 +7,14 @@ import re
 
 html = urllib.urlopen("http://cn.bing.com/").read()
 
-imgAddress = re.search(r"http://.*?\.jpg",html)
+imgAddress = re.search(r'g_img={url: "(.*?)"',html).group(1).replace('\\','')
+
 
 if imgAddress:
 	fileName = "/home/geekgao/图片/BingImg/" + time.strftime("%Y-%m-%d") + ".jpg"
-	print "今天Bing图片的地址是:" + imgAddress.group()
+	print "今天Bing图片的地址是:" + imgAddress
 	print "正在下载……"
-	urllib.urlretrieve(imgAddress.group(), fileName)
+	urllib.urlretrieve(imgAddress, fileName)
 	print "下载完毕!" + "存储为" + fileName
 	orderStr = "gsettings set org.gnome.desktop.background picture-uri \"file:" + fileName + "\""
 	os.system(orderStr)
